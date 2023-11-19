25e2a63a0cc7faa843403a9c86ad2cdb2cc7e4b8
sairajupatta <sairaju.patta123@gmail.com>
2023-11-19 16:06:13 -0700

    Local Vaiable Type Interference example

--- src/main/java/org/personal/Main.java
+++ src/main/java/org/personal/Main.java
@@ -1,26 +1,26 @@
 package org.personal;
 
-
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
-//            Movie movie = new Movie("Star Wars");
-//            Movie.getMovie("Comedy","Saw 2");
-//            movie.watchMovie();
+//        Movie theMovie = Movie.getMovie("Science", "Star Wars");
+//        theMovie.watchMovie();
+
         Scanner s = new Scanner(System.in);
-        while (true){
-            System.out.print(" A C S Q\n");
+        while (true) {
+            System.out.print("Enter Type (A for Adventure, C for Comedy, " +
+                    "S for Science Fiction, or Q to quit): ");
             String type = s.nextLine();
-            if("Qq".contains(type)){
+            if ("Qq".contains(type)) {
                 break;
             }
-            System.out.println("Enter the movie title : ");
+            System.out.print("Enter Movie Title: ");
             String title = s.nextLine();
-            Movie movie = Movie.getMovie(type,title);
+            Movie movie = Movie.getMovie(type, title);
             movie.watchMovie();
         }
-
-        }
     }
+}
